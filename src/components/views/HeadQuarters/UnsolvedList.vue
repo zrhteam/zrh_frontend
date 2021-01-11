976c45fcc83d5e8dad6b935106e5bba0d2a06c7a
12032465 <12032465@mail.sustech.edu.cn>
2021-01-11 18:34:19 +0800

    总部级未整改高风险隐患列表交互

---
+++ src/components/views/HeadQuarters/UnsolvedList.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div style="display: none">
+      {{ getRiskLevelList }}
+    </div>
+    <div id="subtitle" class="level4" style="padding-top: 10px">
+      <span>当前未整改高风险隐患列表</span>
+    </div>
+    <div class="level5" style="padding-bottom: 10px">
+      <el-table
+          :data="riskLevelList"
+          :row-style="{height: '20px'}"
+          :cell-style="{padding: '5px'}"
+          style="width: 100%; color: #93bce7"
+          max-height = '400px'>
+        <el-table-column
+            prop="description"
+            label="隐患描述">
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UnsolvedList",
+  data() {
+    return {
+      riskLevelList: [],
+      timer: ''
+    }
+  },
+  computed: {
+    getRiskLevelList() {
+      let data = this.$store.state.get_headquarter.risk_list;
+      console.log(data)
+      let dataArray = []
+      for (let i in data) {
+        let obj = {
+          description: ''
+        }
+        obj.description = data[i]
+        dataArray.push(obj)
+      }
+      console.log(dataArray)
+      this.riskLevelList = dataArray
+    },
+  },
+  mounted() {
+    this.timer = setInterval(this.updateTable, 1000);
+  },
+  methods: {
+    updateTable() {
+      let first = this.riskLevelList[0];
+      this.riskLevelList.shift();
+      this.riskLevelList.push(first);
+    },
+    beforeDestroy() {
+      clearInterval(this.timer);
+    },
+  }
+}
+</script>
+
+<style scoped>
+/*最外层透明*/
+/deep/ .el-table, /deep/ .el-table__expanded-cell{
+
+  background-color: transparent;
+}
+/* 表格内背景颜色 */
+/deep/ .el-table th,
+/deep/ .el-table tr,
+/deep/ .el-table td {
+
+  background-color: transparent;
+}
+
+</style>
