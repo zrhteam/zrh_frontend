a535382490a39cf74fcbe9cdc89b7c3663455236
12032465 <12032465@mail.sustech.edu.cn>
2021-01-13 23:13:40 +0800

    放了地图

--- src/components/views/Project/PrjOverview.vue
+++ src/components/views/Project/PrjOverview.vue
@@ -41,7 +41,7 @@
         <el-card class="box-card " shadow="never"
                  style="background-color: transparent; height: 24%; margin: 0px 5px 5px 5px">
           <el-button size="mini" round
-                     style="position: absolute; z-index: 9; left: 12%; background-color: transparent; color: #ffffff"
+                     style="z-index: 9; left: 12%; background-color: transparent; color: #ffffff"
                      @click="intoPrjDataScreen">展开
           </el-button>
           <PrjDataScreen id="small1"></PrjDataScreen>
@@ -52,7 +52,12 @@
       <el-col :span="10" class="" style="height: 100%">
         <el-card class="box-card " shadow="never"
                  style="background-color: transparent; height: 79%; margin: 0px 5px 5px 5px">
-          <label>map</label>
+          <!--          放地图-->
+          <div class="map_container" style="height: 100%; width: 100%; z-index:1; background-color: #13E8E9">
+            <div id="map" bordered :dataSource="$store.state.get_locations.data" style="pointer-events:inherit"></div>
+          </div>
+          <!--          <div id="map" bordered style="pointer-events:inherit"></div>-->
+          <!--          <svg style="position: absolute; z-index: 8; width: 100%; height: 100%" pointer-events="none"></svg>-->
         </el-card>
         <!--历次检查指数-->
         <PrjIndex></PrjIndex>
@@ -114,6 +119,7 @@ import IndexView from "@/components/views/HeadQuarters/IndexView.vue";
 import PrjCurrentCorrectionRate from "@/components/views/Project/PrjCurrentCorrectionRate.vue";
 import PrjIndex from "@/components/views/Project/PrjIndex.vue";
 import PrjDataScreen from '@/components/views/Project/PrjDataScreen.vue'
+import * as d3 from "d3/dist/d3";
 
 export default {
   name: "PrjOverview",
@@ -139,6 +145,38 @@ export default {
       this.$refs.tree.filter(val);
     }
 
+  },
+  mounted: function () {
+    this.map = this.loadMap();//加载地图
+    let m = document.getElementById("map")
+    this.map_width = window.getComputedStyle(m).width
+    this.map_height = window.getComputedStyle(m).height
+
+    this.svg = d3.select(this.$el).select('svg');
+    // this.svg = d3.select(this.map.getPanes().overlayPane).append("svg");
+
+    let _this = this;
+    this.map.on('drag', (e) => {
+      let center_position = this.map.latLngToContainerPoint([22, 107]);
+
+      if (this.locContainers) {
+        this.locContainers.each(function (d) {
+          let loc = _this.map.latLngToContainerPoint(d.locs)
+          d3.select(this).attr('transform', 'translate(' + [loc.x, loc.y] + ')');
+        })
+      }
+    });
+
+    this.map.on('move', (e) => {
+      let center_position = this.map.latLngToContainerPoint([22, 107]);
+
+      if (this.locContainers) {
+        this.locContainers.each(function (d) {
+          let loc = _this.map.latLngToContainerPoint(d.locs)
+          d3.select(this).attr('transform', 'translate(' + [loc.x, loc.y] + ')');
+        })
+      }
+    });
   },
   computed: {
     // getTreeData() {
@@ -209,15 +247,44 @@ export default {
       small.style.display = 'block'
       small.style.width = "500px"
       small.style.width = "99%"
-    }
+    },
+    loadMap() {//加载地图
+      let map = L.map("map", {
+        center: [34, 107], // 地图中心
+        zoom: 4, //缩放比列
+        zoomControl: false, //禁用 + - 按钮
+        doubleClickZoom: false, // 禁用双击放大
+        attributionControl: false // 移除右下角leaflet标识
+      });
+      let name = L.tileLayer(
+          // 'https://api.mapbox.com/styles/v1/zhaiyzh/ckes4nsma2yls19op279otef9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemhhaXl6aCIsImEiOiJja2VyeWYzNTYwbHB1MnhzYTV0Z3didG1hIn0.forlrmKVYKXTsyP7voWu9Q'
+          "http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}",//初始化一个 openlayers 地图
+          // 天地图影像图层
+          // "http://t0.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=5d27dc75ca0c3bdf34f657ffe1e9881d", //parent.TiandituKey()为天地图密钥
+          // 天地图影像注记图层
+          // "http://t0.tianditu.com/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=5d27dc75ca0c3bdf34f657ffe1e9881d", //parent.TiandituKey()为天地图密钥
+          // 加载谷歌地图
+          // "http://webrd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8"
+          // 加载高德地图
+          // 'http://webrd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8'
+      ).addTo(map);
+      return map
+    },
   },
 
   data() {
     return {
       filterText: '',
       data: [],
-      status: true
-
+      map: "",
+      dataset: {},
+      form: {
+        name: '',
+      },
+      map_width: 0,
+      map_height: 0,
+      timer: '',
+      input: 'test'
     };
   },
   created() {
@@ -252,17 +319,8 @@ export default {
   display: block;
 }
 
-/*button {*/
-/*  width: 50px;*/
-/*  background-color: #428bca;*/
-/*  border-color: #ffffff;*/
-/*  color: #fff;*/
-/*  border: solid 1px #ffffff;*/
-/*  border-radius: 40px 40px 40px 40px;*/
-/*  -khtml-border-radius: 10px; !* for old Konqueror browsers *!*/
-/*  !*text-align: center;*!*/
-/*  font-weight: 10;*/
-/*  font-size: 10px;*/
-/*  float: right;*/
-/*}*/
+#map {
+  width: 100%;
+  height: calc(100vh);
+}
 </style>
