7d8fe902a474050b1cf7fe9a421b823202036692
12032465 <12032465@mail.sustech.edu.cn>
2021-01-22 20:14:35 +0800

    重构

--- src/components/views/Check/CheckPertangePerc.vue
+++ src/components/views/Check/CheckPertangePerc.vue
@@ -16,6 +16,7 @@
 
 <script>
 import elementResizeDetectorMaker from "element-resize-detector";
+import {bar_option} from "@/utils/constants";
 
 export default {
   name: "CheckPertangePerc",
@@ -50,75 +51,8 @@ export default {
         let arr = this.getData
         console.log("bar_arr", arr)
         if (arr.length) {
-          let option = {
-            tooltip: {},
-            dataset: {
-              dimensions: ['name', 'count'],
-              source: arr
-            },
-            xAxis: {
-              type: 'category',
-              axisLabel: {
-                interval: 0,
-                rotate: 45,
-                textStyle: {
-                  fontSize: 10
-                }
-              },
-              axisLine: {
-                lineStyle: {
-                  color: '#ffffff',
-                  fontSize: 8
-                }
-              }
-            },
-            yAxis: {
-              axisLine: {
-                lineStyle: {
-                  color: '#ffffff'
-                }
-              },
-              axisLabel: {
-                // textStyle: {
-                //   fontSize: 10
-                // }
-              }
-            },
-            series: [
-              {
-                type: 'bar',
-                itemStyle: {
-                  normal: {
-                    //柱形图圆角，初始化效果
-                    barBorderRadius: [10, 10, 0, 0],
-                    color: new echarts.graphic.LinearGradient(
-                        0, 0, 0, 1,
-                        [
-                          {offset: 0, color: '#77b5b8'},
-                          // {offset: 0.5, color: '#1f77a0'},
-                          {offset: 1, color: '#107480'}
-                        ]
-                    )
-                  }
-                },
-                emphasis: {
-                  itemStyle: {
-                    color: '#40abc4'
-                    //     new echarts.graphic.LinearGradient(
-                    //     0, 0, 0, 1,
-                    //     [
-                    //       {offset: 0, color: '#2378f7'},
-                    //       {offset: 0.7, color: '#2378f7'},
-                    //       {offset: 1, color: '#83bff6'}
-                    //     ]
-                    // )
-                  }
-                },
-                barMaxWidth: 40
-              }
-            ]
-          };
-          myChart.setOption(option);
+          bar_option['dataset']['source'] = arr
+          myChart.setOption(bar_option);
         }
         myChart.resize();
         window.addEventListener('resize', function () {
