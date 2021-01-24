7f756776f82f1a390f9fc7ada2ac8634fa4757ca
12032465 <12032465@mail.sustech.edu.cn>
2021-01-24 18:28:22 +0800

    重构

---
+++ src/components/DataCompare.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-row style="height: 100%">
+    <el-col :span="8" style="height: 100%">
+      <el-row style="height: 20%">
+<!--        <div class="grid-content bg-purple">-->
+        <el-col :span="8" style="height: 100%">
+          <label style="font-size: 0.2em; vertical-align: middle; display: inline-block">请先选择粒度</label>
+        </el-col>
+        <el-col :span="16" style="height: 100%">
+          <el-radio-group v-model="radio" @change="ClickRadio">
+          <el-radio v-model="radio" label="1">总部</el-radio>
+          <el-radio v-model="radio" label="2">区域</el-radio>
+          <el-radio v-model="radio" label="3">项目</el-radio>
+          <el-radio v-model="radio" label="4">检查</el-radio>
+            </el-radio-group>
+        </el-col>
+<!--        </div>-->
+      </el-row>
+      <el-row style="height: 80%">
+        <el-col :span="12" style="height: 100%">
+          <el-row style="height: 100%">
+            <Tree
+                :treeObj="treeObj"
+                :show-checkbox="true"
+                @handleNodeClick="handleTrNodeClick"
+                @handleCheck="handleTrCheck"
+                style="height: 100%"></Tree>
+          </el-row>
+        </el-col>
+        <el-col :span="12" style="height: 100%">
+          <el-row style="height: 100%">
+            <Tree
+                :treeObj="treeObj"
+                :show-checkbox="true"
+                @handleNodeClick="handleTrNodeClick"
+                @handleCheck="handleTrCheck"
+                style="height: 100%"></Tree>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-col>
+  <el-col :span="16">
+    <div class="grid-content bg-purple-light"></div>
+  </el-col>
+  </el-row>
+</template>
+
+<script>
+import Tree from "@/components/views/functions/Tree.vue";
+
+export default {
+  name: "DataCompare",
+  components: {Tree},
+  data() {
+    return {
+      treeObj: this.$store.state.get_login.grant_data.data.value,
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      },
+      radio: '1'
+    }
+  },
+  methods: {
+    ClickRadio(radio) {
+      this.radio = radio
+    },
+    handleTrNodeClick(data, node) {
+    },
+    handleTrCheck(a, b, level) {
+      if(level >= this.radio) {
+        this.$message.error('您当前选择的层级与粒度不对应！');
+      }
+    }
+  },
+  created() {
+    //得到树形控件的内容 还负责封装了地理位置信息
+    this.treeObj = this.$store.state.get_login.grant_data.data.value
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/views/functions/Tree.vue
+++ src/components/views/functions/Tree.vue
@@ -14,12 +14,15 @@
             node-key="id"
             :props="defaultProps"
             @node-click="handleNodeClick"
+            @check="handleCheck"
             :default-expanded-keys="expandedKeys"
             :filter-node-method="filterNode"
             accordion
-            default-expand-all
+            :show-checkbox="showCheckbox"
+            highlight-current
+            @node-expand="treeExpand"
             :expand-on-click-node="true"
-            ref="tree">
+            ref="modelTree">
                                 <span class="span-ellipsis" slot-scope="{ node, data }">
                                   <span :title="node.label">{{ node.label }}</span>
                                 </span>
@@ -35,6 +38,9 @@ export default {
   props: {
     treeObj: {
       type: Object
+    },
+    showCheckbox: {
+      type: Boolean
     }
   },
   data() {
@@ -147,7 +153,6 @@ export default {
         }
       }
       this.$store.commit('get_login/changePosition', {params: pp})
-      console.log("arr", arr)
       this.data = arr
       // this.$store.state.get_login.tree_data = arr
       // this.p_data = p_arr
@@ -157,219 +162,15 @@ export default {
       return data.label.indexOf(value) !== -1;
     },
     handleNodeClick(data, node) {
-      // console.log("节点", data);
-      // console.log(node);
       this.expandedKeys.push(data.id)
-      if (this.user_grant === '项目') {
-        if ((node.level == 1) || (node.level == 2)) {
-          alert("您没有权限")
-        } else if (node.level == 3) {
-          this.prjNodeClick(data, node)
-          document.getElementById('prj_subpart').style.display = 'block'
-          document.getElementById('check_part').style.display = 'none'
-          document.getElementById('map_1').style.display = 'none'
-          document.getElementById('map_2').style.display = 'block'
-          document.getElementById('prj_charts').style.display = 'block'
-          document.getElementById('check_charts').style.display = 'none'
-        } else if (node.level == 4) {
-          this.checkNodeClick(data, node)
-          //首先要判断当前是在数据大屏页面还是在主页面
-          if (document.getElementById("prj_small").style.display === 'none') {//在主页面
-            document.getElementById("prj_subpart").style.display = 'none'
-            document.getElementById("prj_charts").style.display = 'none'
-            document.getElementById("check_charts").style.display = 'block'
-            document.getElementById("check_part").style.display = 'block'
-          } else {
-            document.getElementById("prj_subpart").style.display = 'none'
-            document.getElementById("prj_charts").style.display = 'none'
-            document.getElementById("check_charts").style.display = 'block'
-            document.getElementById("check_part").style.display = 'block'
-            // document.getElementById("large1").style.display = 'none'
-            // document.getElementById("large2").style.display = 'none'
-          }
-        }
-      } else if (this.user_grant === '区域') {
-        if (node.level == 1) {
-          alert("您没有权限")
-        } else if (node.level == 2) {
-          this.regionNodeClick(data, node)
-          document.getElementById('region').style.display = 'block'
-          document.getElementById('prj_part').style.display = 'none'
-        } else if (node.level == 3) {//区域=》项目
-          this.prjNodeClick(data, node)
-          //首先要判断当前是在数据大屏页面还是在主页面=》不用再判断了，大屏页面没有树形控件
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'block'
-          document.getElementById("check_part").style.display = 'none'
-        } else if (node.level == 4) {//区域=》检查
-          this.checkNodeClick(data, node)
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'none'
-          document.getElementById("check_part").style.display = 'block'
-        }
-      } else if (this.user_grant === '总部') {
-        if (node.level == 1) {//总部=》总部
-          this.headNodeClick(data, node)
-          document.getElementById('head_quarter').style.display = 'block'
-          document.getElementById('region_part').style.display = 'none'
-        } else if (node.level == 2) {//总部=》区域
-          this.regionNodeClick(data, node)
-          // console.log("检查", data)
-          // console.log(node)
-          //首先要判断当前是在数据大屏页面还是在主页面=>不需要再判断了，大屏页面不要树形控件
-          document.getElementById('head_quarter').style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'block'
-          document.getElementById('prj_part').style.display = 'none'
-          //为了画出多边形，要重新封装该区域的数据
-          let r_p = [];
-          for(let i in data['children']) {
-            // alert(data['children'][i])
-            console.log(data['children'][i]['pos'])
-          }
-          var container = L.DomUtil.get('map_3'); if(container != null){ container._leaflet_id = null; }
-          this.map = new L.map("map_3");
-          // this.map.setView([30,30], 6)
-          // this.map = L.map("map_3", {
-          //   center: [34, 107], // 地图中心
-          //   zoom: 4, //缩放比列
-          //   zoomControl: false, //禁用 + - 按钮
-          //   // doubleClickZoom: false, // 禁用双击放大
-          //   attributionControl: false // 移除右下角leaflet标识
-          // });
-          // this.map.panTo(data.pos[0], data.pos[1])
-        } else if (node.level == 3) {//总部=》项目
-          this.prjNodeClick(data, node)
-          document.getElementById("head_quarter").style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'block'
-          document.getElementById('check_part').style.display = 'none'
-        } else if (node.level == 4) {//总部=》检查
-          this.checkNodeClick(data, node)
-          document.getElementById("head_quarter").style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'none'
-          document.getElementById('check_part').style.display = 'block'
-        }
-      }else if(this.user_grant="超级用户") {
-        this.headNodeClick(data, node)
-        if (node.level == 1) {//总部=》总部
-          this.headNodeClick(data, node)
-          document.getElementById('head_quarter').style.display = 'block'
-          document.getElementById('region_part').style.display = 'none'
-        } else if (node.level == 2) {//总部=》区域
-          this.regionNodeClick(data, node)
-          // console.log("检查", data)
-          // console.log(node)
-          //首先要判断当前是在数据大屏页面还是在主页面=>不需要再判断了，大屏页面不要树形控件
-          document.getElementById('head_quarter').style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'block'
-          document.getElementById('prj_part').style.display = 'none'
-          //为了画出多边形，要重新封装该区域的数据
-          let r_p = [];
-          for(let i in data['children']) {
-            // alert(data['children'][i])
-            console.log(data['children'][i]['pos'])
-          }
-          var container = L.DomUtil.get('map_3'); if(container != null){ container._leaflet_id = null; }
-          this.map = new L.map("map_3");
-          // this.map.setView([30,30], 6)
-          // this.map = L.map("map_3", {
-          //   center: [34, 107], // 地图中心
-          //   zoom: 4, //缩放比列
-          //   zoomControl: false, //禁用 + - 按钮
-          //   // doubleClickZoom: false, // 禁用双击放大
-          //   attributionControl: false // 移除右下角leaflet标识
-          // });
-          // this.map.panTo(data.pos[0], data.pos[1])
-        } else if (node.level == 3) {//总部=》项目
-          this.prjNodeClick(data, node)
-          document.getElementById("head_quarter").style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'block'
-          document.getElementById('check_part').style.display = 'none'
-        } else if (node.level == 4) {//总部=》检查
-          this.checkNodeClick(data, node)
-          document.getElementById("head_quarter").style.display = 'none'
-          document.getElementById('region_part').style.display = 'block'
-          document.getElementById('region').style.display = 'none'
-          document.getElementById('prj_part').style.display = 'block'
-          document.getElementById("prj_subpart").style.display = 'none'
-          document.getElementById('check_part').style.display = 'block'
-        }
-      }
+      this.$emit('handleNodeClick', data, node)
     },
-    headNodeClick(data, node) {
-      let param = new URLSearchParams();
-      param.append('headquarter_name', data.label);
-      this.$store.commit('get_headquarter/changeParams', {params: param})
-      this.$store.dispatch('get_headquarter/getInitRectification')
-      this.$store.dispatch('get_headquarter/getInitRiskLevelData')
-      this.$store.dispatch('get_headquarter/getInitRiskNumberRank')
-      this.$store.dispatch('get_headquarter/getInitImage')
-      this.$store.dispatch('get_headquarter/getInitNumberTop')
-      this.$store.dispatch('get_headquarter/getInitRiskList')
-      this.$store.dispatch('get_headquarter/getInitRiskIndexData')
+    handleCheck(a, b) {
+      this.$emit('handleCheck', a, b, this.$refs.modelTree.getNode(a.id).level)
     },
-    regionNodeClick(data, node) {
-      let param = new URLSearchParams();
-      param.append('region_name', data.label);
-      this.$store.commit('get_region/changeParams', {params: param})
-      this.$store.dispatch('get_region/getInitRegionProjectNumber')
-      this.$store.dispatch('get_region/getInitRegionRiskLevel')
-      this.$store.dispatch('get_region/getInitRegionHighRisk')
-      this.$store.dispatch('get_region/getInitRegionImage')
-      this.$store.dispatch('get_region/getInitRegionMajor')
-      this.$store.dispatch('get_region/getInitRegionNumberTop')
-      // this.$store.dispatch('get_region/getInitRegionSafetyIndex')
-      this.$store.dispatch('get_region/getInitRegionRiskRank')
-    },
-    prjNodeClick(data, node) {
-      let param = new URLSearchParams();
-      param.append('project_name', data.label);
-      this.$store.commit('get_project/changeParams', {params: param})
-      this.$store.dispatch('get_project/getInitProjectRectification')
-      this.$store.dispatch('get_project/getInitProjectRiskLevel')
-      this.$store.dispatch('get_project/getInitProjectHistoryPerception')
-      this.$store.dispatch('get_project/getInitProjectNumberChange')
-      // // 当前未整改高风险隐患列表
-      this.$store.dispatch('get_project/getInitPrjRisk')
-      // // 当前未整改高风险隐患图片
-      this.$store.dispatch('get_project/getInitProjectImage')
-      //
-      // //占比
-      this.$store.dispatch('get_project/getInitProjectSystem')
-      this.$store.dispatch('get_project/getInitProjectRegionDistribution')
-      this.$store.dispatch('get_project/getInitProjectReason')
-      //  历次检查中出现次数排前5的隐患描述及其所属专业和出现次数
-      this.$store.dispatch('get_project/getInitProjectRiskTop')
-      // this.map.setZoom(4)
-      // setTimeout(function () {
-      //   this.map.panTo(new L.LatLng(34, 107));
-      // }, 100)
-    },
-    checkNodeClick(data, node) {
-      let param1 = new URLSearchParams();
-      param1.append('check_code', data.label);
-      this.$store.commit('get_check/changeParams', {params: param1})
-      this.$store.dispatch('get_check/getCheckRectification')
-      this.$store.dispatch('get_check/getCheckRiskLevel')
-      this.$store.dispatch('get_check/getCheckRiskRatio')
-      this.$store.dispatch('get_check/getCheckHighRisk')
-      this.$store.dispatch('get_check/getCheckHighImage')
-      this.$store.dispatch('get_check/getCheckMajorSystem')
-      this.$store.dispatch('get_check/getCheckMajorArea')
-      this.$store.dispatch('get_check/getCheckMajorStage')
-      this.$store.dispatch('get_check/getCheckRiskTop')
+    treeExpand(data, node, self) {
+      // console.log("add",this.expandedKeys)
+      this.expandedKeys.push(data.id)
     }
   },
   created() {
