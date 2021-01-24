578087f4a31dec03a43a050f54b7a1cd5db2ae50
12032465 <12032465@mail.sustech.edu.cn>
2021-01-24 10:21:12 +0800

    重构

--- src/base.js
+++ src/base.js
@@ -1,6 +1,31 @@
 // 写全局函数
 exports.install = function (Vue, options) {
-    Vue.prototype.prjNodeClick = function (project_name) {//全局函数1
+    Vue.prototype.headNodeClick = function (headquarter_name) {//全局函数1,点击树形控件的总部，查询该总部的大屏信息
+      let param = new URLSearchParams();
+      param.append('headquarter_name', headquarter_name);
+      this.$store.commit('get_headquarter/changeParams', {params: param})
+      this.$store.dispatch('get_headquarter/getInitRectification')
+      this.$store.dispatch('get_headquarter/getInitRiskLevelData')
+      this.$store.dispatch('get_headquarter/getInitRiskNumberRank')
+      this.$store.dispatch('get_headquarter/getInitImage')
+      this.$store.dispatch('get_headquarter/getInitNumberTop')
+      this.$store.dispatch('get_headquarter/getInitRiskList')
+      this.$store.dispatch('get_headquarter/getInitRiskIndexData')
+    };
+    Vue.prototype.regionNodeClick = function (region_name) {//全局函数2,点击树形控件的区域，查询该区域的大屏信息
+      let param = new URLSearchParams();
+      param.append('region_name', region_name);
+      this.$store.commit('get_region/changeParams', {params: param})
+      this.$store.dispatch('get_region/getInitRegionProjectNumber')
+      this.$store.dispatch('get_region/getInitRegionRiskLevel')
+      this.$store.dispatch('get_region/getInitRegionHighRisk')
+      this.$store.dispatch('get_region/getInitRegionImage')
+      this.$store.dispatch('get_region/getInitRegionMajor')
+      this.$store.dispatch('get_region/getInitRegionNumberTop')
+      // this.$store.dispatch('get_region/getInitRegionSafetyIndex')
+      this.$store.dispatch('get_region/getInitRegionRiskRank')
+    };
+    Vue.prototype.prjNodeClick = function (project_name) {//全局函数3,点击树形控件的项目，查询该项目的大屏信息
         let param = new URLSearchParams();
         param.append('project_name', project_name);
         this.$store.commit('get_project/changeParams', {params: param})
@@ -23,7 +48,7 @@ exports.install = function (Vue, options) {
         //   this.map.panTo(new L.LatLng(34, 107));
         // }, 100)
     };
-    Vue.prototype.checkNodeClick = function (check_code) {//全局函数2
+    Vue.prototype.checkNodeClick = function (check_code) {//全局函数4,点击树形控件的检查，查询该检查的大屏信息
         let param1 = new URLSearchParams();
         param1.append('check_code', check_code);
         this.$store.commit('get_check/changeParams', {params: param1})
